1132f6c241bd49ebe1129b85d84e43d33bc8226f
Felix <felixrunye@gmail.com>
2021-02-17 17:50:54 +0300

    added login and main page layout pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('logout', [
+    'as' => 'logout',
+    function () {
+        Auth::logout();
+        return redirect()->route('main');
+    }
+]);
+
+Route::get('/client/{any?}', function () {
+    if(Auth::user() == null){
+        return redirect()->route('main');
+    }else{
+        return view('pages.main');
+    }
+});
+
+Route::get('/', function () {
+    return view('pages.login');
+})->name('main');
